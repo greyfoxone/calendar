ba0814e61a453949af2711557098ee88b9be702d
greyfoxone <woj.mueller@gmx.de>
2026-02-20 18:03:08 +0100

    further encapsulation

--- main.py
+++ main.py
@@ -11,6 +11,41 @@ from PyQt6.QtWidgets import QVBoxLayout
 from PyQt6.QtWidgets import QWidget
 
 
+class CurrentDay(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update)
+        self.timer.start(60000)
+        self.update()
+
+    def init_ui(self):
+        layout = QVBoxLayout()
+        self.date_label = QLabel()
+        self.date_label.setStyleSheet(
+            """
+            margin-top: 11px;
+            font-size: 60%;
+        """
+        )
+        self.weekday_label = QLabel()
+        self.weekday_label.setStyleSheet(
+            """
+            color: #cc6666;
+            font-size: 40%;
+        """
+        )
+        layout.addWidget(self.date_label)
+        layout.addWidget(self.weekday_label)
+        self.setLayout(layout)
+
+    def update(self):
+        now = datetime.now()
+        self.date_label.setText(now.strftime("%Y-%m-%d"))
+        self.weekday_label.setText(now.strftime("%A"))
+
+
 class TimeLabel(QLabel):
     def __init__(self):
         super().__init__()
@@ -21,10 +56,11 @@ class TimeLabel(QLabel):
         """
         )
         self.timer = QTimer(self)
-        self.timer.timeout.connect(self.update_time)
+        self.timer.timeout.connect(self.update)
         self.timer.start(60000)
+        self.update()
 
-    def update_time(self):
+    def update(self):
         now = datetime.now()
         self.setText(now.strftime("%H:%M"))
 
@@ -34,7 +70,6 @@ class ClockCalendarDesklet(QWidget):
     def __init__(self):
         super().__init__()
         self.init_ui()
-        self.update_clock()
 
     def init_ui(self):
         self.create_ui()
@@ -43,45 +78,19 @@ class ClockCalendarDesklet(QWidget):
 
     def create_ui(self):
         self.time_label = TimeLabel()
-
-        self.date_label = QLabel()
-        self.date_label.setStyleSheet(
-            """
-            margin-top: 11px;
-            font-size: 60%;
-        """
-        )
-
-        self.weekday_label = QLabel()
-        self.weekday_label.setStyleSheet(
-            """
-            color: #cc6666;
-            font-size: 40%;
-        """
-        )
-
+        self.current_day = CurrentDay()
         self.calendar_table = CalendarTableWidget()
 
     def layout_ui(self):
         layout = QVBoxLayout()
 
         clock_layout = QHBoxLayout()
-        date_layout = QVBoxLayout()
-
-        date_layout.addWidget(self.date_label)
-        date_layout.addWidget(self.weekday_label)
-
         clock_layout.addWidget(self.time_label)
-        clock_layout.addLayout(date_layout)
+        clock_layout.addWidget(self.current_day)
         layout.addLayout(clock_layout)
 
         layout.addWidget(self.calendar_table)
         self.setLayout(layout)
-
-    def update_clock(self):
-        now = datetime.now()
-        self.date_label.setText(now.strftime("%Y-%m-%d"))
-        self.weekday_label.setText(now.strftime("%A"))
 
 
 if __name__ == "__main__":
